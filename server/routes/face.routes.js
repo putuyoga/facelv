75b44d8a2f1f33b5e4880615d35f1f88733e8df3
I Putu Yoga Permana <Putu_P@mitrais.com>
2016-09-21 15:26:11 +0800

    add several new route for the api

--- server/routes/face.routes.js
+++ server/routes/face.routes.js
@@ -5,8 +5,9 @@ const router = new Router();
 router.route('/faces').get(FaceController.getFaces);
 router.route('/faces/:faceId').get(FaceController.getFace);
 router.route('/faces').post(FaceController.addFace);
+router.route('/faces/:faceId').put(FaceController.updateFaces);
 router.route('/faces/:faceId').delete(FaceController.deleteFace);
 router.route('/faces/:faceId/yay').post(FaceController.yayFace);
 router.route('/faces/:faceId/nay').post(FaceController.nayFace);
-
+router.route('/faces/best/:limit').get(FaceController.bestFace);
 export default router;

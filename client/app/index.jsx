6c2aeb59d963d0399bc9bb9648237c99db996c31
I Putu Yoga Permana <Putu_P@mitrais.com>
2016-09-21 15:24:57 +0800

    change the structure and use route

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -1,6 +1,32 @@
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import ReactDOM from 'react-dom';
 import React from 'react';
+import { Router, Route, IndexRoute, Link, IndexLink, browserHistory } from 'react-router'
+const ACTIVE = { color: 'red' }
+
+class App extends React.Component {
+  render() {
+    return  (
+      <div className="container">
+        <header className="page-header">
+          <h1><small>face</small> vote</h1>
+        </header>
+        <nav className="navbar">
+          <div className="container-fluid">
+            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-9">
+              <ul className="nav navbar-nav">
+                <li><IndexLink to="/" activeStyle={ACTIVE}>VOTE</IndexLink></li>
+                <li><Link to="/rank" activeStyle={ACTIVE}>RANK</Link></li>
+                <li><Link to="/log" activeStyle={ACTIVE}>LOG</Link></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        {this.props.children}
+      </div>
+    );
+  }
+}
 
 class FaceboxItem extends React.Component {
   constructor() {
@@ -115,13 +141,13 @@ class Facebox extends React.Component {
     super();
     this.state = {
       items: [],
-      source: ""
+      source: "/api/faces"
     };
   }
 
   // fetch data from our API
   componentDidMount() {  
-    fetch(this.props.source)
+    fetch(this.state.source)
     .then((response) => response.json())
     .then((json) => {
       this.setState({items: json});
@@ -136,17 +162,77 @@ class Facebox extends React.Component {
     var boxItems = [];
     var items = this.state.items;
     for(var i = 0; i < items.length; i++) {
-      boxItems.push(<FaceboxItem data={items[i]} key={items[i]._id} source={this.props.source}/>)
+      boxItems.push(<FaceboxItem data={items[i]} key={items[i]._id} source={this.state.source}/>)
     }
     //console.log(this.state.items.count);
     return <div className="faceBox">{boxItems}</div>
   }
 }
+class RankItem extends React.Component {
+  render() {
+    return(
+      <tr>
+        <td className="col-md-1 col-sm-2 text-center"><img className="img-circle img-responsive media-object" src={this.props.data.image} /></td>
+        <td className="col-md-8 col-sm-7 text-left"><h4><strong>{this.props.data.name}</strong></h4></td>
+        <td className="col-md-1 col-sm-1 text-center"><h4><span className="label label-primary">+{this.props.data.yay}</span></h4></td>
+        <td className="col-md-1 col-sm-1 text-center"><h4><span className="label label-danger">-{this.props.data.nay}</span></h4></td>
+        <td className="col-md-1 col-sm-1 text-center"><h4><span className="label label-default">{this.props.data.total}</span></h4></td>
+      </tr>
+    );
+  }
+}
 
-ReactDOM.render(
-  <Facebox source="/api/faces"/>,
-  document.getElementById('content')
-);
+class Rank extends React.Component {
+  constructor() {
+      super();
+      this.state = {
+        items: [],
+        source: "/api/faces"
+      };
+  }
 
+  // fetch data from our API
+  componentDidMount() {  
+    fetch(this.state.source + '/best/5')
+    .then((response) => response.json())
+    .then((json) => {
+      this.setState({items: json});
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+  }
 
+  render() {
+    var rankItems = [];
+    var items = this.state.items;
+    for(var i = 0; i < items.length; i++) {
+      rankItems.push(<RankItem data={items[i]} key={items[i]._id}/>)
+    }
+    var thead = <thead><tr><th></th><th>name</th><th className="text-center">❤</th><th className="text-center">✖</th><th className="text-center">=</th></tr></thead>;
+    return (
+      <table className="table">{thead}<tbody>{rankItems}</tbody></table>
+    );
+  }
+}
 
+class NotFound extends React.Component {
+  render() {
+    return (
+      <h2>404 😛</h2>
+    );
+  }
+}
+
+// Declarative route configuration (could also load this config lazily
+// instead, all you really need is a single root route, you don't need to
+// colocate the entire config).
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Facebox} />
+      <Route path="rank" component={Rank} />
+      <Route path="*" component={NotFound}/>
+    </Route>
+  </Router>
+), document.getElementById('content'))

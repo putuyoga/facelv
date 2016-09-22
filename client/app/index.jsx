ed0de6e72cf0cedbd50620bebd5d41dccba27bea
I Putu Yoga Permana <IPutuYoga.Permana@mitrais.com>
2016-09-22 15:40:23 +0800

    included css to be bundled along

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -8,6 +8,10 @@ import Facebox from './components/Facebox';
 import Rank from './components/Rank';
 import NotFound from './components/NotFound';
 
+
+// Base stylesheet
+require('./main.css');
+
 // Declarative route configuration (could also load this config lazily
 // instead, all you really need is a single root route, you don't need to
 // colocate the entire config).

cccf8ce5fa23421a8ec1f694f131f44a1e6b2b1b
I Putu Yoga Permana <IPutuYoga.Permana@mitrais.com>
2016-09-26 15:38:33 +0800

    clean up mongoURL

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://putuyoga:11071993@ds046549.mlab.com:46549/facelvdb' || 'mongodb://localhost:27017/fldv',
+  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/fldv',
   port: process.env.PORT || 6969,
 };
 

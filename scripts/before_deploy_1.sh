0eae2950c4f4e60361fa304726e804b26738428b
I Putu Yoga Permana <IPutuYoga.Permana@mitrais.com>
2016-09-28 09:53:07 +0800

    updated script

--- scripts/before_deploy_1.sh
+++ scripts/before_deploy_1.sh
@@ -1,8 +1,12 @@
 # we need bundle our assets
 echo "Build Webpack!"
 npm build
+
 # remove the last line of .gitignore
 # we need the `public` directory on deployment
 echo "Remove Last Line of .gitignore!"
 sed -i '$ d' .gitignore
-cat .gitignore
+
+#updated git
+git add .
+git commit

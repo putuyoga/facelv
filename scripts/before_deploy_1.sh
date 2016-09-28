56e29e3deaa694d28dbf88394e6332e9be1ae17b
I Putu Yoga Permana <IPutuYoga.Permana@mitrais.com>
2016-09-28 09:15:08 +0800

    updated build script

--- scripts/before_deploy_1.sh
+++ scripts/before_deploy_1.sh
@@ -1,3 +1,5 @@
+# we need bundle our assets
+npm build
 # remove the last line of .gitignore
 # we need the `public` directory on deployment
 head -n -1 .gitignore > temp_gitignore ; mv temp_gitignore .gitignore

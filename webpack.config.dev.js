8afc2f27a738a62dc17bd191464f71c4774c86a6
I Putu Yoga Permana <IPutuYoga.Permana@mitrais.com>
2016-09-30 15:50:08 +0800

    new webpack conf both dev and production

---
+++ webpack.config.dev.js
@@ -0,0 +1,41 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var DIST_DIR = path.resolve(__dirname, 'dist');
+var APP_DIR = path.resolve(__dirname, 'client');
+var config = {
+  entry: {
+    app: [APP_DIR + '/index.js'],
+    vendor: ['react', 'react-dom', 'react-router', 'react-addons-css-transition-group']
+  },
+  output: {
+    path: DIST_DIR,
+    filename: 'app.js'
+  },
+  module : {
+    loaders : [
+      {
+        test : /\.jsx?$/,
+        exclude: /node_module/,
+        loader : 'babel'
+      },
+      {
+        test: /\.css$/,
+        
+        loader: 'style-loader!css-loader?localIdentName=[name]__[local]__[hash:base64:5]&modules&importLoaders=1&sourceMap!postcss-loader',
+      }
+    ]
+  },
+  resolve : {
+      extensions: ['', '.js', '.jsx']
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: Infinity,
+      filename: 'vendor.js'
+    })
+  ]
+};
+
+module.exports = config;

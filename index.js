de102bd3bbe627312a0c3e1ab8b2209ca33dc1b5
I Putu Yoga Permana <IPutuYoga.Permana@mitrais.com>
2016-09-30 15:52:38 +0800

    split production and development

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-// todo: do something
-require('babel-register');
-require('./server/server');
+/**
+ * Entry Script
+ */
+
+if(process.env.NODE_ENV === 'production') {
+    // production environment
+    console.log('we are run on production server');
+    process.env.webpackAssets = JSON.stringify(require('./dist/manifest.json'));
+    process.env.webpackChunkAssets = JSON.stringify(require('./dist/chunk-manifest.json'));
+    require('./dist/server.bundle.js');
+} else {
+    // development environment
+    require('babel-register');
+    require('./server/server');
+}

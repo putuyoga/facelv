74fc251a290da7368a82c271cb73e12991e48356
I Putu Yoga Permana <IPutuYoga.Permana@mitrais.com>
2016-10-12 16:36:59 +0800

    add secret key to config

--- server/config.js
+++ server/config.js
@@ -1,7 +1,9 @@
 const config = {
   mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/fldv',
   port: process.env.PORT || 6969,
-  appName: 'faceLv'
+  appName: 'faceLv',
+  secretKey: 'faceLvKey',
+  jwtSession: { session: false }
 };
 
 export default config;
